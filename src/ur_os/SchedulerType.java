4aecf96f64fea212ac964bff22c3e9c67dd70297
Pedro Wightman <pedro.wightman@urosario.edu.co>
2025-02-20 10:22:41 -0500

    SchedulerType Enum

---
+++ src/ur_os/SchedulerType.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package ur_os;
+
+/**
+ *
+ * @author prestamour
+ */
+public enum SchedulerType {
+    FCFS,
+    SJF_NP,
+    SJF_P,
+    RR,
+    PRIORITY,
+    MFQ,
+    FAIR
+}
