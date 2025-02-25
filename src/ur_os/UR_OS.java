280202cc392293428d515870a4bbe5050524931a
Pedro Wightman <pedro.wightman@urosario.edu.co>
2025-02-25 09:41:48 -0500

    Update UR_OS.java

--- src/ur_os/UR_OS.java
+++ src/ur_os/UR_OS.java
@@ -11,7 +11,7 @@ package ur_os;
  */
 public class UR_OS {
 
-    private static String VERSION = "0.0.3.8.3";
+    private static String VERSION = "0.0.3.8.4";
     
     /**
      * @param args the command line arguments
